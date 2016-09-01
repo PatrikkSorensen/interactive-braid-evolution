266a8698aa9109665789b489fba803eb129f06e1
patrikk sørensen <patrikk srensen>
2016-09-01 15:03:36 +0200

    Begun working on obj reading

---
+++ unity/interactive-braid-evolution/Assets/Scripts/ObjImporter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjImporter : MonoBehaviour {
+
+    public string objFileName;
+    public string objFileName2;
+
+    void Start()
+    {
+        objFileName = Application.dataPath + "/ObjReader/Sample Files/" + objFileName;
+        ObjReader.use.ConvertFile(objFileName, false); 
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Y))
+        {
+            objFileName = "";
+            objFileName = Application.dataPath + "/ObjReader/Sample Files/" + objFileName2;
+            ObjReader.use.ConvertFile(objFileName, false);
+        }
+    }
+}
