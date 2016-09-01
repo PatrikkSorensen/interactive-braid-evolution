ad483fc0fcf230d22d5234d89d7706733cc7e228
patrikk sørensen <patrikk srensen>
2016-09-01 13:42:14 +0200

    Clean up and model exporting from GH

--- unity/interactive-braid-evolution/Assets/Scripts/UDPContinousBoxes.cs
+++ unity/interactive-braid-evolution/Assets/Scripts/UDPContinousBoxes.cs
@@ -10,68 +10,34 @@ using System.Threading;
 public class UDPContinousBoxes : MonoBehaviour
 {
 
+	public int port;
+    private string IP;
+    private IPEndPoint remoteEndPoint;
+	private UdpClient client;
 
-	private string IP;  
-	public int port;  
-	public bool mouseOverBox = false;
-	GameObject lastHitBox;
-	
-	// "connection" things
-	IPEndPoint remoteEndPoint;
-	UdpClient client;
-
-	// Use this for initialization
 	void Start ()
 	{
+        // Has to be declared in start 
+        IP = "10.13.1.52";
+        port = 8051;
 
-		IP = "172.20.10.3";
-		port = 8051;
-		
-
-		remoteEndPoint = new IPEndPoint (IPAddress.Parse (IP), port);
-		client = new UdpClient ();
-		
-		// status
+        remoteEndPoint = new IPEndPoint (IPAddress.Parse (IP), port);
+        client = new UdpClient();
 		Debug.Log ("Sending to " + IP + " : " + port);
 	}
-	
-	// Update is called once per frame
+
 	void Update ()
 	{
 
+        //TODO: Make this send user interaction 
         if(Input.GetKeyDown(KeyCode.X)) { 
-            sendString("key : 'value'");
-            }
-
-
-   //     if (!Input.GetMouseButton (1)) {
-			//Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			//RaycastHit hit;
-
-			//if (Physics.Raycast (ray, out hit)) {
-			//	//if (lastHitBox != hit.collider.gameObject) {
-			//	//	if (lastHitBox != null)
-			//	//		lastHitBox.GetComponent<MeshRenderer> ().enabled = true;
-			//	//	lastHitBox = hit.collider.gameObject;
-			//	//	lastHitBox.GetComponent<MeshRenderer> ().enabled = false;
-			//	//}
-			//	mouseOverBox = true;
-
-			//} else {
-			//	mouseOverBox = false;
-			//	if (lastHitBox != null)
-			//		lastHitBox.GetComponent<MeshRenderer> ().enabled = true;
-			//	lastHitBox = null;
-			//}
-		//}
+            sendString("Message from Unity!");
+        }
 	}
 
-	// a funtion to send data via UDP
 	private void sendString (string message)
 	{
 		try {
-
-			
 			// encode string to UTF8-coded bytes
 			byte[] data = Encoding.UTF8.GetBytes (message);
 			
