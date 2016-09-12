eb3f639bf0e33f424973316cab10941499183d6a
patrikk sørensen <patrikk srensen>
2016-09-12 13:21:07 +0200

    Initial work on user interface and debugging controls

---
+++ unity/interactive-braid-evolution/Assets/Scripts/user interface/DebugWindow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugWindow : MonoBehaviour {
+
+    public KeyCode debugKey = KeyCode.T;
+
+    private bool toggle = true; 
+    private GameObject debugWindow; 
+	
+	void Start () {
+        debugWindow = GameObject.FindGameObjectWithTag("DebugWindow");     
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(debugKey))
+        {
+            Debug.Log("Toggle: " + toggle); 
+            debugWindow.SetActive(!toggle);
+            toggle = !toggle; 
+        }
+    }
+}
---
+++ unity/interactive-braid-evolution/Assets/Scripts/user interface/SliderUpdater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class SliderUpdater : MonoBehaviour {
+
+    private Text txt; 
+	// Use this for initialization
+	void Start () {
+        txt = GetComponentInChildren<Text>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        txt.text = "hello world"; 
+	}
+}
