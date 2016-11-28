77548f99f33f82bee88946bdc62eb113c700a033
PatrikkSorensen <patri-kk@hotmail.com>
2016-11-28 16:03:38 +0100

    Begun working on better exit loop through animation

---
+++ unity/interactive-braid-evolution/Assets/ExitManagerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitManagerScript : MonoBehaviour {
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GameObject.Find("FormCanvas").GetComponent<Animator>(); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            anim.SetTrigger("advance"); 
+    }
+}
