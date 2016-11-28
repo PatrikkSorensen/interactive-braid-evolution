8c7eb59a18f481e89c27829af1f33ae69fcec347
PatrikkSorensen <patri-kk@hotmail.com>
2016-11-28 15:11:21 +0100

    Integrated splash screen

---
+++ unity/interactive-braid-evolution/Assets/Scripts/utility/MySceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour {
+
+    public void LoadSelectedScene()
+    {
+        SceneManager.LoadScene(SplashScreenManager.sceneIndex); 
+    }
+
+}
---
+++ unity/interactive-braid-evolution/Assets/Scripts/utility/SplashScreenManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenManager : MonoBehaviour {
+
+    private Animator anim;
+    public static int sceneIndex; 
+
+    void Start()
+    {
+        sceneIndex = 0; 
+        anim = GameObject.Find("SplashCanvas").GetComponent<Animator>(); 
+    }
+
+	public void ChangeLevel(int levelIndex)
+    {
+        sceneIndex = levelIndex; 
+        anim.SetTrigger("fadeout");
+    }
+}
