a36e59b263f9137707bcaec7f303e4e7087f89b1
PatrikkSorensen <patri-kk@hotmail.com>
2016-11-28 18:24:31 +0100

    Started working on animation for tuorial

---
+++ unity/interactive-braid-evolution/Assets/Scripts/utility/TutorialManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour
+{
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GameObject.Find("TutorialCanvas").GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            anim.SetTrigger("advance");
+    }
+}
