9de4bd7d73a760099261f552cb909f168faa78d5
patrikk sørensen <patrikk srensen>
2016-11-28 23:49:36 +0100

    Made a simple xml parser

---
+++ unity/interactive-braid-evolution/Assets/Scripts/utility/GifLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GifLoader : MonoBehaviour {
+
+    Texture2D[] frames;
+    RawImage image; 
+    float framesPerSecond;
+
+
+    void Start()
+    {
+        image = GetComponent<RawImage>(); 
+    }
+
+	void Update () {
+        int index =  (int) (Time.time * framesPerSecond);
+
+        index = index % frames.Length;
+        image.texture = frames[index];
+    }
+}
---
+++ unity/interactive-braid-evolution/Assets/Scripts/utility/XMLFormularWriter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class XMLFormularWriter : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+            SaveInfo(); 
+    }
+
+
+    void SaveInfo() {
+        XmlDocument doc = new XmlDocument();
+        XmlElement el = (XmlElement)doc.AppendChild(doc.CreateElement("info"));
+   
+        el.AppendChild(doc.CreateElement("name")).InnerText = "placeholder goes here";
+        el.AppendChild(doc.CreateElement("description")).InnerText = "A lot of placeholder text will be added here";
+        el.AppendChild(doc.CreateElement("feedback")).InnerText = "A lot of feedback is gonna be right here";
+        el.AppendChild(doc.CreateElement("email")).InnerText = "Email is gonna be right here bitches";
+
+        string path = Application.dataPath + "/Resources/";
+ 
+        doc.Save(path + "file.xml");
+        Debug.Log("Saved xml to: " + path); 
+    }
+}
