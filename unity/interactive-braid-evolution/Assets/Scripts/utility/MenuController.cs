ebec2d17f5bf07d735652bcd367c117c0413e636
patrikk sørensen <patrikk srensen>
2016-12-01 20:35:34 +0100

    Never too late to make a user controller and main menu

---
+++ unity/interactive-braid-evolution/Assets/Scripts/utility/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+
+    public static void ToggleMenu()
+    {
+        GameObject menu = GameObject.FindGameObjectWithTag("MainMenu");
+        menu.GetComponent<Animator>().SetTrigger("fade"); 
+    }
+}
---
+++ unity/interactive-braid-evolution/Assets/UserController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserController : MonoBehaviour {
+
+    public GameObject mainMenu; 
+    float speed; 
+
+	// Use this for initialization
+	void Start () {
+        mainMenu.GetComponent<MenuController>(); 
+        speed = 0.25f; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            MenuController.ToggleMenu(); 
+
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Debug.Log("h: " + hor + ", v: " + ver); 
+        Vector3 v = Camera.main.transform.forward * ver; 
+        transform.Translate(v * speed);
+
+        Vector3 v2 = Camera.main.transform.right * hor;
+        Debug.Log(v2); 
+        transform.Translate(v2 * speed);
+    }
+}
