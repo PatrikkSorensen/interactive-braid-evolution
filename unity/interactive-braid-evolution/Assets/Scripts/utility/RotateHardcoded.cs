9fe5ea3c602778c6995c11ad90e62a4aa609426f
PatrikkSorensen <patri-kk@hotmail.com>
2016-12-01 16:03:17 +0100

    Finally done with the tutorial

---
+++ unity/interactive-braid-evolution/Assets/Scripts/utility/RotateHardcoded.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateHardcoded : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * 15 * Time.deltaTime);
+    }
+}
---
+++ unity/interactive-braid-evolution/Assets/Scripts/utility/showcase/ChangeBraidMaterial.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeBraidMaterial : MonoBehaviour {
+
+    public GameObject b1;
+    public GameObject b2;
+    public GameObject b3;
+
+    public Material newMaterial;
+
+    private Material startMaterial;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+            ChangeMaterialOfBraid1(); 
+    }
+    private void Start()
+    {
+        startMaterial = b1.GetComponentInChildren<Renderer>().material; 
+    }
+
+    public void ChangeMaterialOfBraid1()
+    {
+        b1.GetComponentInChildren<Renderer>().material = newMaterial;
+        Debug.Log("Hello world"); 
+    }
+
+    public void ChangeMaterialOfBraid2()
+    {
+        b2.GetComponentInChildren<Renderer>().material = newMaterial;
+        Debug.Log("Changing material");
+    }
+
+    public void ChangeMaterialOfBraid3()
+    {
+        b3.GetComponentInChildren<Renderer>().material = newMaterial;
+        Debug.Log("Changing material");
+    }
+}
