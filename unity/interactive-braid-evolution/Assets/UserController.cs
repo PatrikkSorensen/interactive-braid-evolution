cb9f61e820a684da41323debe8e324a51bc932ca
PatrikkSorensen <patri-kk@hotmail.com>
2016-12-02 10:57:40 +0100

    Change animation folder structure and begun working on keyboard animation controller for the keyboard scene

--- unity/interactive-braid-evolution/Assets/UserController.cs
+++ unity/interactive-braid-evolution/Assets/UserController.cs
@@ -11,15 +11,13 @@ public class UserController : MonoBehaviour {
         mainMenu.GetComponent<MenuController>(); 
         speed = 0.25f; 
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Escape))
             MenuController.ToggleMenu(); 
 
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
-        Debug.Log("h: " + hor + ", v: " + ver); 
         Vector3 v = Camera.main.transform.forward * ver; 
         transform.Translate(v * speed);
 
