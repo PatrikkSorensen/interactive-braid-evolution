3affa6644e2ed2bfd431807cadc8988d0f867862
PatrikkSorensen <patri-kk@hotmail.com>
2016-12-06 11:04:40 +0100

    moved placeholder prefabs to the prefabs folder and added sprinting

--- unity/interactive-braid-evolution/Assets/UserController.cs
+++ unity/interactive-braid-evolution/Assets/UserController.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using System;
 
 public class UserController : MonoBehaviour {
 
     public GameObject mainMenu; 
-    float speed; 
+    float speed;
+    float sprintSpeed; 
+    bool sprint; 
+
 
 	// Use this for initialization
 	void Start () {
         mainMenu.GetComponent<MenuController>(); 
-        speed = 0.25f; 
+        speed = 0.25f;
+        sprintSpeed = 2.0f;
 	}
 
 	void Update () {
@@ -18,14 +23,35 @@ public class UserController : MonoBehaviour {
             MenuController.ToggleMenu();
 
         if (Input.GetKeyDown(KeyCode.R))
-            SceneManager.LoadScene(3); 
+            SceneManager.LoadScene(3);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            FindObjectOfType<BraidSimulationManager>().SetFlagsFromButton(false); 
+        AddTranslation(); 
+
+    }
+
+    private void AddTranslation()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            sprint = true;
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+            sprint = false;
 
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
-        Vector3 v = Camera.main.transform.forward * ver; 
-        transform.Translate(v * speed);
-
+        Vector3 v = Camera.main.transform.forward * ver;
         Vector3 v2 = Camera.main.transform.right * hor;
-        transform.Translate(v2 * speed);
+
+        if (sprint)
+        {
+            transform.Translate(v2 * speed * 2);
+            transform.Translate(v * speed * 2);
+        }
+        else
+        {
+            transform.Translate(v2 * speed);
+            transform.Translate(v * speed);
+        }
     }
 }
