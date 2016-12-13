670c8f4b9e0af6f098f3682a6fb14b5f934353ab
PatrikkSorensen <patri-kk@hotmail.com>
2016-12-13 16:09:49 +0100

    Started working on making the leap motion scene

---
+++ unity/interactive-braid-evolution/Assets/ScrollBraidViewer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI; 
+using System.Collections;
+
+public class ScrollBraidViewer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.X))
+            Scroll(); 
+	}
+
+    void Scroll()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Debug.Log(h); 
+        Scrollbar scrollbar = FindObjectOfType<Scrollbar>();
+        scrollbar.value += h * Time.deltaTime; 
+    }
+}
